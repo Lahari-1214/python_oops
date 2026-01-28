782b1311d2be4d02ed204ac3b0e54718fee54941
Leela <leelanarayan1214@gmail.com>
2026-01-28 14:38:55 +0530

    Polymorphism introduction

---
+++ Introduction/polymorphism.py
@@ -0,0 +1,9 @@
+# poly means many and morphos means forms --> one object or method acts in different forms is said to be polymorphism
+
+# a function that exhibits polymorphism 
+def add(a, b): 
+    print(a+b) 
+# call add() and pass two integers 
+add(5, 10)  # displays 15 
+# call add() and pass two strings 
+add("Core", "Python")  # displays CorePython 
