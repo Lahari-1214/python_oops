b3b87caf430484b0648cf882327f72b05f80cbc7
Leela <leelanarayan1214@gmail.com>
2026-01-31 20:58:02 +0530

    How to excute super class methods without overriding methods in sub class

---
+++ Inheritance/super.py
@@ -0,0 +1,13 @@
+# super() is a built-in method which is useful to call the super class constructor or methods from the sub class.
+
+# super() can be used as:
+'''
+super().__init__()     
+# call super class constructor 
+super().__init__(arguments)# call super class constructor and pass  
+# arguments  
+super().method()  
+ # call super class method 
+''' 
+
+
