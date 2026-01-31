40fff92a25262e14c5983ee4583d8a959eabc445
Leela <leelanarayan1214@gmail.com>
2026-01-31 20:29:01 +0530

    The next question is why the base class members are automatically available to sub class?

--- Inheritance/creating_inherit.py
+++ Inheritance/creating_inherit.py
@@ -1,6 +1,7 @@
 
 # sub class acquiring properties of base class
 # Inheritance allows a class to reuse the attributes and methods of another class, reducing code duplication and improving maintainability.
+#  syntax: class Subclass(Baseclass):  
 
 class Animal:        # Parent class
     def sound(self):
@@ -15,3 +16,6 @@ d.sound()   # inherited method
 d.bark()
 
 # Benefits: code reusablity, cleaner code,Easy maintance
+
+# The next question is why the base class members are automatically available to sub class? 
+# When a child class inherits from a parent class, it automatically gains access to all public and protected members (attributes and methods) of the parent class. This is because the child class is essentially an extension of the parent class, inheriting its structure and behavior.
