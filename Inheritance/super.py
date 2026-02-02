0374c8d61e92ff2fefd97bf2f0ef82ff09051019
Leela <leelanarayan1214@gmail.com>
2026-02-02 16:53:10 +0530

    Example for super()

--- Inheritance/super.py
+++ Inheritance/super.py
@@ -10,4 +10,22 @@ super().method()
  # call super class method 
 ''' 
 
-
+# accessing base class constructor in sub class 
+class Father: 
+    def __init__(self, property=0): 
+        self.property = property 
+ 
+    def display_property(self): 
+        print('Father\'s property= ', self.property) 
+ 
+class Son(Father):     
+    def __init__(self, property1=0, property=0): 
+        super().__init__(property)    
+        self.property1= property1  
+ 
+    def display_property(self): 
+        print('Total property of child= ', self.property1 + self.property) 
+ 
+# create sub class instance and display father's property 
+s = Son(200000.00, 800000.00) 
+s.display_property() 
