2db0c4c06cfebeb31289099e4813a85c7936964a
Leela <leelanarayan1214@gmail.com>
2026-02-06 09:14:58 +0530

    Multiple Inheritance

--- Inheritance/types_of_inheritance.py
+++ Inheritance/types_of_inheritance.py
@@ -1,4 +1,4 @@
-# 5 types of inheritances 1.Single inheritance 2.Multilevel Inheritance 3.Multiple Inheritance 4.Hierarchical Inheritance 5.Hybrid Inheritance
+# 5 types of inheritances 1.Single inheritance 2.Multiple Inheritance 3.Multilevel Inheritance  4.Hierarchical Inheritance 5.Hybrid Inheritance
 
 # Single inheritance:Deriving one or more sub classes from a single base class is called ‘single inheritance’.
 class Bank(object):
@@ -23,3 +23,28 @@ a.available_cash()
 s = Statebank()
 s.available_cash()
     
+
+
+# Multiple Inheritance:Deriving sub classes from multiple (or more than one) base classes is called ‘multiple inheritance’. 
+# Syntax:class Subclass(Baseclass1, Baseclass2, … ): 
+
+class Father: 
+    def height(self): 
+        print('Height is 6.0 foot') 
+ 
+class Mother: 
+    def color(self): 
+        print('Color is brown') 
+ 
+class Child(Father, Mother): 
+    pass 
+ 
+c = Child() 
+print('Child\'s inherited qualities: ') 
+c.height() 
+c.color() 
+
+# problems of using multiple inheritance: all the constructors of super class doesn't available to the sub class for the purpose we have Method Resolution order (MRO) which is described in MRO.py
+
+
+
